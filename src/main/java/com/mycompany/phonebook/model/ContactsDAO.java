8d52aa1a44096ae9d8d1819d79661fd9b67e64f1
Kupryk Michael <brcjkee@gmail.com>
2017-04-04 21:32:00 +0300

    Added SELECT methods to the ContactsDAO.java.

--- src/main/java/com/mycompany/phonebook/model/ContactsDAO.java
+++ src/main/java/com/mycompany/phonebook/model/ContactsDAO.java
@@ -1,6 +1,8 @@
 package com.mycompany.phonebook.model;
 
 import com.mycompany.phonebook.util.DBUtil;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -44,4 +46,43 @@ public class ContactsDAO {
         }
         return contacts;
     }
+
+    public static ObservableList<Contacts> searchContacts () throws SQLException, ClassNotFoundException{
+        String selectStmnt = "SELECT * FROM phonebook.contacts";
+        try {
+            ResultSet resultSet = DBUtil.dbExecuteQuery(selectStmnt);
+            ObservableList<Contacts> contactsList = getContactsList(resultSet);
+            return contactsList;
+        } catch (SQLException e){
+            System.out.println("SQL select operation has been failed: " + e);
+            throw e;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static ObservableList<Contacts> getContactsList(ResultSet resultSet) throws SQLException, ClassNotFoundException{
+        ObservableList<Contacts> contactsList = FXCollections.observableArrayList();
+        while(resultSet.next()){
+            Contacts contacts = new Contacts();
+            contacts = new Contacts();
+            contacts.setContact_id(resultSet.getInt("contact_id"));
+            contacts.setContact_name(resultSet.getString("contact_name"));
+            contacts.setOrganization(resultSet.getString("organization"));
+            contacts.setGroup(resultSet.getString("group"));
+            contacts.setMobile_number(resultSet.getString("mobile_number"));
+            contacts.setOffice_number(resultSet.getString("office_number"));
+            contacts.setHome_number(resultSet.getString("home_number"));
+            contacts.setFax_number(resultSet.getString("fax_number"));
+            contacts.setEmail(resultSet.getString("email"));
+            contacts.setWeb(resultSet.getString("web"));
+            contacts.setOther_cont(resultSet.getString("other_cont"));
+            contacts.setOther_cont2(resultSet.getString("other_cont2"));
+            contacts.setAdress(resultSet.getString("adress"));
+            contactsList.add(contacts);
+        }
+        return contactsList;
+    }
+    
 }
