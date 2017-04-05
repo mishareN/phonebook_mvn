db2521f44b36ee559b71eb20e88bcf187d2cdb7c
Kupryk Michael <brcjkee@gmail.com>
2017-04-05 21:36:11 +0300

    Bind and declare fx:id values for all form elements. Some methods for these elements have been added.

--- src/main/java/com/mycompany/phonebook/controller/Controller.java
+++ src/main/java/com/mycompany/phonebook/controller/Controller.java
@@ -1,11 +1,21 @@
 package com.mycompany.phonebook.controller;
 
+import javafx.scene.control.Alert;
+
 /**
  * Created by Michael Kupryk on 29.03.2017.
  */
 public class Controller {
-    public void logInButtonClicked ()
+    public void handleExit ()
     {
-        System.out.println("User Logged in...");
+        System.exit(0);
+    }
+
+    public void handleHelp() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Help");
+        alert.setHeaderText("Phonebok. (JavaFX + MySQL)");
+        alert.setContentText("The program is designed to store and quickly access information about your contacts.");
+        alert.show();
     }
 }
